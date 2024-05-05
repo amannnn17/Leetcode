b121a8ad4a287d1e355b2ab6a3bf80536b6e04d2
amannnn17 <164628999+amannnn17@users.noreply.github.com>
2024-05-05 16:25:29 +0530

    Time: 3 ms (96.05%), Space: 8.9 MB (99.84%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-        bool isvowel(char a){
-             a=tolower(a); 
-             return (a=='a' || a=='e' || a=='i' || a=='o' || a=='u');
-        }
+    bool isvowel(char a) {
+        a = tolower(a);
+        return (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u');
+    }
     string reverseVowels(string s) {
-      
-        int f=0,l=s.size()-1;
-        while(f<l){
-            if(isvowel(s[f]) && isvowel(s[l])){
-                swap(s[f],s[l]);
+
+        int f = 0, l = s.size() - 1;
+        while (f < l) {
+            if (isvowel(s[f]) && isvowel(s[l])) {
+                swap(s[f], s[l]);
                 f++;
                 l--;
-            }
-            else if (isvowel(s[f]) && !isvowel(s[l])) l--;
-            else if(!isvowel(s[f]) && isvowel(s[l])) f++;
-            else{
+            } else if (isvowel(s[f]) && !isvowel(s[l]))
+                l--;
+            else if (!isvowel(s[f]) && isvowel(s[l]))
+                f++;
+            else {
                 f++;
                 l--;
             }
